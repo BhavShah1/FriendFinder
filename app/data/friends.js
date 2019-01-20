bcb19ba960b333355cc5412b379cbedace2660b3
BhavShah1 <bhavin.d.shah1@gmail.com>
2019-01-19 23:17:35 -0800

    final

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+var matches = [
+    {
+    // "name": "Paulie",
+    // "photo":"https://m.media-amazon.com/images/M/MV5BMjA4NTcyNTA2NV5BMl5BanBnXkFtZTcwODkyMDY0Nw@@._V1_.jpg",
+    "name": "",
+    "photo": "",
+    "scores":[
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1,
+        // 1
+      ]
+
+  },
+]
+
+module.exports = matches;
